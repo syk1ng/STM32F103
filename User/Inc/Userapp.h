fbafe52fa9abc981537d579b4c37e1b199750b43
shuyongkang <q1179726098@gmail.com>
2024-07-03 00:58:57 +0800

    init

---
+++ User/Inc/Userapp.h
@@ -0,0 +1,9 @@
+//
+// Created by 11797 on 2024/7/2.
+//
+
+#ifndef USERAPP_H
+#define USERAPP_H
+
+void UserApp(void);
+#endif //USERAPP_H
---
+++ User/Src/Userapp.c
@@ -0,0 +1,13 @@
+//
+// Created by 11797 on 2024/7/2.
+//
+
+#include "Userapp.h"
+#include <stdio.h>
+
+#include "usart.h"
+
+
+void UserApp() {
+    printf("123\r\n");
+}
