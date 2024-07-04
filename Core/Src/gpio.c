43f44a25fef8c6083963382d2d5ad230becee980
shuyongkang <q1179726098@gmail.com>
2024-07-04 22:53:06 +0800

    feature:学习了如何用按键gpio中断控制led亮灭

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -20,6 +20,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "gpio.h"
+#include "time.h"
 
 /* USER CODE BEGIN 0 */
 
@@ -51,7 +52,13 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_SET);
+
+  /*Configure GPIO pin : PC13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PC2 PC3 */
   GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3;
@@ -60,15 +67,40 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
+  /*Configure GPIO pin : PA0 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI0_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI0_IRQn);
+
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+
 }
 
 /* USER CODE BEGIN 2 */
 void LedOpen(enum led_pin pin) {
-    	HAL_GPIO_WritePin(GPIOC, pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOC, pin, GPIO_PIN_RESET);
 }
 
-
 void LedClose(enum led_pin pin) {
-    	HAL_GPIO_WritePin(GPIOC, pin, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(GPIOC, pin, GPIO_PIN_SET);
+}
+
+void LedToggle(enum led_pin pin) {
+  HAL_GPIO_TogglePin(GPIOC, pin);
+}
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
+  delay_ms(30);
+  if (GPIO_Pin == GPIO_PIN_0) {
+    LedToggle(GPIO_PIN_2);
+  } else if (GPIO_Pin == GPIO_PIN_13) {
+    LedToggle(GPIO_PIN_3);
+  }
 }
 /* USER CODE END 2 */
